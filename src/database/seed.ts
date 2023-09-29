38d289f207382175aa69f519b96d6afec52e285d
Mustafa Salem <mustafaissasalem@gmail.com>
2023-09-29 21:51:14 +0300

    feat: add a check for command line argument to run the seed with the cli only
    Close: #13

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,9 +1,10 @@
+import mongoose from 'mongoose';
+import yargs from 'yargs';
+
 import { Countries } from '../models';
 import { parseBinaryUri, fetchCountries, reshapeCountriesData, updateCountriesFile } from '../utils';
 import { configs } from '../configs';
 import { connection } from './connection';
-
-const { nodeEnv } = configs;
 
 export const seed = async () => {
   try {
@@ -18,16 +19,25 @@ export const seed = async () => {
     await updateCountriesFile(reshapedCountries);
 
     // ? Empty the database before seeding.
-    await Countries.deleteMany({}, { maxTimeMS: 60000 });
+    await Countries.deleteMany({});
 
     // ? Seed the database with the reshaped countries array.
     await Countries.create(reshapedCountries);
+
+    await mongoose.disconnect();
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log('Something went wrong while seeding the database, ', error);
   }
 };
 
-if (nodeEnv !== 'test') {
-  seed();
+if (require.main === module) {
+  yargs.command(
+    'seed', // ? The command-line argument we need to recognize
+    'Seeding the database', // ? Description of the command.
+    {}, // ? Empty configuration object.
+    async () => {
+      await seed();
+    }, // ? Call back function that will be called in the success case.
+  ).argv;
 }

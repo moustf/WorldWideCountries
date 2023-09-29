a2db4e6cefba409afe7ca37736030e5c42e39801
Mustafa Salem <mustafaissasalem@gmail.com>
2023-09-29 21:52:36 +0300

    feat: made the queries use the newly created text index for searching
    Close: #13

--- src/queries/countries/getAllCountries.ts
+++ src/queries/countries/getAllCountries.ts
@@ -2,5 +2,7 @@ import { Countries } from '../../models';
 
 export const getAllCountriesQuery = async (query: string) =>
   Countries.find({
-    $or: [{ 'name.official': query }, { 'name.common': query }, { cca2: query }, { cca3: query }, { ccn3: query }],
+    $text: {
+      $search: query,
+    },
   }).exec();
--- src/queries/countries/getCountryCurrencies.ts
+++ src/queries/countries/getCountryCurrencies.ts
@@ -3,7 +3,9 @@ import { Countries } from '../../models';
 export const getCountryCurrenciesQuery = async (query: string) =>
   Countries.find(
     {
-      cca2: query,
+      $text: {
+        $search: query,
+      },
     },
     {
       currencies: 1,
